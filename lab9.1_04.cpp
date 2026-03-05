ec377f22b39a6c1a4d7abf48b204f3861dd7b322
s6804062612285 Warakorn <s6804062612285@email.kmutnb.ac.th>
2026-03-06 04:07:02 +0700

    Commit 1> lab9no4.c:swap 3 values

---
+++ lab9.1_04.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swapValue(int *a, int *b, int *c);
+int main() {
+    int a = 1, b = 2, c = 3;
+    
+    printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
+    swapValue(&a, &b, &c);
+    
+    printf("After swap function : a=%d, b=%d, c=%d\n", a, b, c);
+    return 0;
+}
+
+void swapValue(int *a, int *b, int *c) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = *c;
+    *c = temp;
+}
